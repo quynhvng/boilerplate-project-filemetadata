505dc5623745a31428b9ad50acb460e01641ab29
Quỳnh Vương <43884073+quynhvng@users.noreply.github.com>
2023-09-15 14:00:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var formidable = require('formidable');
 require('dotenv').config()
 
 var app = express();
@@ -8,10 +9,24 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// file API
+app
+.route('/api/fileanalyse')
+.post(function (req, res, next) {
+  const form = formidable({ multiples: true });
 
+  form.parse(req, (err, fields, files) => {
+    if (err) next();
+    res.json({
+      name: files.upfile.originalFilename,
+      type: files.upfile.mimetype,
+      size: files.upfile.size
+    });
+  })
+})
 
 
 const port = process.env.PORT || 3000;
